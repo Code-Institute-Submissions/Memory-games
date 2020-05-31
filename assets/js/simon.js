a4c90493615dd9e19ac3f29f39bacd57c5b92090
joenapper <joe.napper@hotmail.co.uk>
2020-05-31 13:19:33 +0000

    Added working simon game

---
+++ assets/js/simon.js
@@ -0,0 +1,57 @@
+const topLeft = document.querySelector('.top-left-panel');
+const topRight = document.querySelector('.top-right-panel');
+const bottomRight = document.querySelector('.bottom-right-panel');
+const bottomLeft = document.querySelector('.bottom-left-panel');
+
+
+
+const getRandomPanel = () => {
+    const panels = [topLeft, topRight, bottomRight, bottomLeft]
+    return panels[parseInt(Math.random() * panels.length)];
+};
+
+const sequence = [getRandomPanel()];
+let sequenceToGuess = [...sequence];
+
+const flash = panel => {
+	return new Promise((resolve, reject) => {
+		panel.className += ' active';
+		setTimeout(() => {
+			panel.className = panel.className.replace(
+				' active',
+				''
+            );
+            setTimeout(() => {
+            resolve();
+            },250)
+		}, 900);
+	});
+};
+
+let canClick = false;
+
+const panelClicked = panelClicked => {
+    if(!canClick) return;
+    const expectedPanel = sequenceToGuess.shift();
+    if (expectedPanel === panelClicked) {
+        if(sequenceToGuess.length === 0) {
+            //Start New Round
+            sequence.push(getRandomPanel());
+            sequenceToGuess = [...sequence];
+            startFlashing();
+        }
+    } else {
+        //End Game
+        alert('GAME OVER!')
+    }
+}
+
+const startFlashing = async () => {
+    canClick = false;
+    for (const panel of sequence) {
+        await flash(panel);
+    };
+    canClick = true;
+}
+
+startFlashing();
