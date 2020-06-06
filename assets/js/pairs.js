e82d9b98592e7bdc495c3a475e5139d90a3aa233
joenapper <joe.napper@hotmail.co.uk>
2020-06-06 18:41:06 +0000

    Bug fix: spam clicking on button

--- assets/js/pairs.js
+++ assets/js/pairs.js
@@ -1,10 +1,13 @@
 const cards = document.querySelectorAll('.card');
 
 let hasCardFlipped = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 //When card is clicked, card color appears
 function onCardClicked() {
+    if (lockBoard) return;
+    if(this === firstCard) return;
     this.classList.replace('color-hidden', 'sample'); //change .replace to .toggle and remove '.'
 
     if(!hasCardFlipped) {
@@ -14,23 +17,33 @@ function onCardClicked() {
     } else {
         //Second click / Card 2
         hasCardFlipped = false;
-        secondCard = this
+        secondCard = this;
 
-        //If cards match
-        if (firstCard.dataset.color === secondCard.dataset.color) {
-            firstCard.removeEventListener('click', onCardClicked);
-            secondCard.removeEventListener('click', onCardClicked);
-        } else {
-            //If cards DON'T match
-            
-            setTimeout(() => {
-                firstCard.classList.add('color-hidden');
-                secondCard.classList.add('color-hidden')
-            }, 500);
-        }
-        
+        checkForMatch();
     }
 }
+
+function checkForMatch() {
+    //If cards match
+    if (firstCard.dataset.color === secondCard.dataset.color) {
+        firstCard.removeEventListener('click', onCardClicked);
+        secondCard.removeEventListener('click', onCardClicked);
+    } else {
+    //If cards DON'T match
+    lockBoard = true;
+        setTimeout(() => {
+            firstCard.classList.add('color-hidden');
+            firstCard.classList.remove('sample');
+            secondCard.classList.add('color-hidden');
+            secondCard.classList.remove('sample');
+            lockBoard = false;
+        }, 500);
+    }      
+}
+
+//function playAgain {
+    //if
+//}
 
 cards.forEach(card => card.addEventListener('click', onCardClicked))
 
