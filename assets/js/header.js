fced3103019bff27a89394c3f2bc696b47abd408
joenapper <joe.napper@hotmail.co.uk>
2020-06-17 21:15:35 +0000

    Refactoring header js code

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,42 +1,46 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li')
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+const header = document.querySelector('header');
 
+// Toggle Nav
+function navSlide() {
 
-    const header = document.querySelector('header');
-window.addEventListener('scroll', () => {
-    const scrollPos = window.scrollY;
-    if(scrollPos > 10){
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-})
-
-    
-    burger.addEventListener('click',()=>{
-        //toggle nav
+    burger.addEventListener('click', () => {
+        
+        // Overlay Slide In
         nav.classList.toggle('nav-active');
 
-        ///animate links
-    navLinks.forEach((link, index)=> {
-        if(link.style.animation) {
-            link.style.animation = ''
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-        }
-    });
-    //burger animation
-    burger.classList.toggle('burger-toggle')
+        // Links Slide In
+        linkSlide();
+
+        // Burger Animation
+        burger.classList.toggle('burger-toggle');
+        
     });
 };
 
+// Links Slide In
+function linkSlide() {
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+        };
+    });
+}; 
+
 navSlide()
 
-//turn into larger call function
+// Adds Background Color
+window.addEventListener('scroll', () => {
 
-//const app = ()=>{
-//    navSlide();
-//    scroll()
-//}
+    const scrollPos = window.scrollY;
+
+    if(scrollPos > 10) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    };
+});
