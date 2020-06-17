adf6c4864c981625111f729aa70269c00e437a5a
joenapper <joe.napper@hotmail.co.uk>
2020-06-17 21:07:37 +0000

    Fixing nav bar bug with more modern design

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,10 @@
-const header = document.querySelector('.main-header');
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+
+    const header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     const scrollPos = window.scrollY;
     if(scrollPos > 10){
@@ -7,3 +13,30 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 })
+
+    
+    burger.addEventListener('click',()=>{
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        ///animate links
+    navLinks.forEach((link, index)=> {
+        if(link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+        }
+    });
+    //burger animation
+    burger.classList.toggle('burger-toggle')
+    });
+};
+
+navSlide()
+
+//turn into larger call function
+
+//const app = ()=>{
+//    navSlide();
+//    scroll()
+//}
